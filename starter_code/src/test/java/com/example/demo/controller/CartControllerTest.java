e155768f948f1d7dffa2ff89a267bcf92f0f5e49
anhdungg <ledung.ht17@gmail.com>
2023-07-19 22:52:53 +0700

    First commit

---
+++ starter_code/src/test/java/com/example/demo/controller/CartControllerTest.java
@@ -0,0 +1,154 @@
+package com.example.demo.controller;
+
+import com.example.demo.controllers.CartController;
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CartControllerTest {
+	@Autowired
+	CartController cartController;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	CartRepository cartRepository;
+	@Autowired
+	ItemRepository itemRepository;
+
+	void setUp() {
+		userRepository = mock(UserRepository.class);
+		cartRepository = mock(CartRepository.class);
+		itemRepository = mock(ItemRepository.class);
+		cartController = new CartController(userRepository, cartRepository, itemRepository);
+		setupCart();
+	}
+
+	private void setupCart() {
+		User user = new User();
+		Cart cart = new Cart();
+		user.setId(0);
+		user.setUsername("test");
+		user.setPassword("testPassword");
+		user.setCart(cart);
+		when(userRepository.findByUsername("test")).thenReturn(user);
+
+		Item item = new Item();
+		item.setId(1L);
+		item.setName("Americano");
+		BigDecimal price = BigDecimal.valueOf(2.99);
+		item.setPrice(price);
+		item.setDescription("Espresso with water");
+		when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
+
+	}
+
+	@Test
+	public void addToCartOK() {
+		setUp();
+		ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(1L);
+		modifyCartRequest.setQuantity(1);
+		modifyCartRequest.setUsername("test");
+		ResponseEntity<Cart> response = cartController.addToCart(modifyCartRequest);
+
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+		Cart cart = response.getBody();
+		assertNotNull(cart);
+		assertEquals(BigDecimal.valueOf(2.99), cart.getTotal());
+	}
+
+	@Test
+	public void addToCartWrongUser() {
+		setUp();
+		ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(1L);
+		modifyCartRequest.setQuantity(1);
+		modifyCartRequest.setUsername("username");
+		ResponseEntity<Cart> response = cartController.addToCart(modifyCartRequest);
+
+		assertNotNull(response);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void addToCartNotFoundItem() {
+		setUp();
+		ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(2L);
+		modifyCartRequest.setQuantity(1);
+		modifyCartRequest.setUsername("test");
+		ResponseEntity<Cart> response = cartController.addToCart(modifyCartRequest);
+
+		assertNotNull(response);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void removeFromCartOK() {
+		setUp();
+		// Set up test by adding two items to cart.
+		ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(1L);
+		modifyCartRequest.setQuantity(2);
+		modifyCartRequest.setUsername("test");
+		ResponseEntity<Cart> response = cartController.addToCart(modifyCartRequest);
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+
+		modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(1L);
+		modifyCartRequest.setQuantity(1);
+		modifyCartRequest.setUsername("test");
+		response = cartController.removeFromCart(modifyCartRequest);
+
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+		Cart cart = response.getBody();
+		assertNotNull(cart);
+		assertEquals(BigDecimal.valueOf(2.99), cart.getTotal());
+
+	}
+
+	@Test
+	public void removeFromCartInvalidUser() {
+		setUp();
+		ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(1L);
+		modifyCartRequest.setQuantity(1);
+		modifyCartRequest.setUsername("username");
+		ResponseEntity<Cart> response = cartController.removeFromCart(modifyCartRequest);
+
+		assertNotNull(response);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void removeFromCartInvalidItem() {
+		setUp();
+		ModifyCartRequest modifyCartRequest = new ModifyCartRequest();
+		modifyCartRequest.setItemId(2L);
+		modifyCartRequest.setQuantity(1);
+		modifyCartRequest.setUsername("test");
+		ResponseEntity<Cart> response = cartController.removeFromCart(modifyCartRequest);
+
+		assertNotNull(response);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+
+}
---
+++ starter_code/src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -0,0 +1,106 @@
+package com.example.demo.controller;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.example.demo.controllers.UserController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+
+import java.util.Optional;
+
+public class UserControllerTest {
+	@Autowired
+	UserController userController;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	CartRepository cartRepository;
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@BeforeEach
+	void setUp() {
+		userRepository = mock(UserRepository.class);
+		cartRepository = mock(CartRepository.class);
+		bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
+		userController = new UserController(userRepository, cartRepository, bCryptPasswordEncoder);
+		setupUser();
+	}
+
+	private void setupUser() {
+		User user = new User();
+		Cart cart = new Cart();
+		user.setId(0);
+		user.setUsername("test");
+		user.setPassword("testPassword");
+		user.setCart(cart);
+		when(userRepository.findByUsername("test")).thenReturn(user);
+		when(userRepository.findById(0L)).thenReturn(Optional.of(user));
+		when(userRepository.findByUsername("user")).thenReturn(null);
+	}
+
+	@Test
+	void testCreateUserValidRequest() {
+		// Arrange
+		when(bCryptPasswordEncoder.encode("testPassword")).thenReturn("hashPassword");
+		CreateUserRequest createUserRequest = new CreateUserRequest();
+		createUserRequest.setUsername("test");
+		createUserRequest.setPassword("testPassword");
+		createUserRequest.setConfirmPassword("testPassword");
+		final ResponseEntity<User> response = userController.createUser(createUserRequest);
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+		User user = response.getBody();
+		assertNotNull(user);
+		assertEquals(0, user.getId());
+		assertEquals("test", user.getUsername());
+		assertEquals("hashPassword", user.getPassword());
+	}
+
+	@Test
+	public void findUserByNameNotFound() {
+		final ResponseEntity<User> response = userController.findByUserName("user");
+		assertNotNull(response);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+
+	@Test
+	public void findUserByIdSuccess() {
+		final ResponseEntity<User> response = userController.findById(0L);
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+		User user = response.getBody();
+		assertNotNull(user);
+		assertEquals(0, user.getId());
+		;
+	}
+
+	@Test
+	public void findUserByNameSuccess() {
+		final ResponseEntity<User> response = userController.findByUserName("test");
+		assertNotNull(response);
+		assertEquals(200, response.getStatusCodeValue());
+		User user = response.getBody();
+		assertNotNull(user);
+		assertEquals("test", user.getUsername());
+	}
+
+	@Test
+	public void findUserByIdNotFound() {
+		final ResponseEntity<User> response = userController.findById(1L);
+		assertNotNull(response);
+		assertEquals(404, response.getStatusCodeValue());
+	}
+}
